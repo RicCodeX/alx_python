7c7895bdc486d2138fe314a8becee3120abafa29
timirichy <tianchris198@gmail.com>
2023-11-06 19:54:13 +0100

    new

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,15 +2,18 @@
 def fibonacci_sequence(n):
     if n <= 0:
         return []
+    elif n == 1:
+        return [0]
+    else:
+        fibonacci = [0, 1]
+        while len(fibonacci) < n:
+            next_number = fibonacci[-1] + fibonacci[-2]
+            fibonacci.append(next_number)
+        return fibonacci
 
-    fibonacci_numbers = [0, 1]
-    while len(fibonacci_numbers) < n:
-        next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
-        fibonacci_numbers.append(next_number)
-
-    return fibonacci_numbers
-
-# Test the function
-n = 10
-fib_numbers = fibonacci_sequence(n)
-print(fib_numbers)
+# Test the function with the provided examples
+if __name__ == "__main__":
+    print(fibonacci_sequence(6))
+    print(fibonacci_sequence(1))
+    print(fibonacci_sequence(0))
+    print(fibonacci_sequence(20))
